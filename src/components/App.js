53810bf8b038f1c6e9c7d75f760b8df4fd53881d
ertucode <ertugrulsirtt@gmail.com>
2022-07-25 13:25:28 +0300

    Delete working

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,47 @@
-import React from "react"
-import RecipeList from "./RecipeList"
-import "../css/app.css"
+import React, { useState } from "react";
+import RecipeList from "./RecipeList";
+import "../css/app.css";
+
+const { v4: uuidv4 } = require("uuid");
+
+export const RecipeContext = React.createContext();
 
 function App() {
+  const [recipes, setRecipes] = useState(sampleRecipes);
+
+  const recipeContextValue = {
+    handleRecipeAdd,
+    handleRecipeDelete,
+  };
+
+  function handleRecipeAdd() {
+    const newRecipe = {
+      id: uuidv4(),
+      name: "new",
+      servings: 1,
+      cookTime: "1:00",
+      instructions: "Instr.",
+      ingredients: [
+        {
+          id: uuidv4(),
+          name: "Name",
+          amount: "1 Tbs",
+        },
+      ],
+    };
+
+    setRecipes([...recipes, newRecipe]);
+  }
+
+  function handleRecipeDelete(id) {
+    setRecipes(recipes.filter((recipe) => recipe.id !== id));
+  }
+
   return (
-    <>
-      <RecipeList recipes={sampleRecipes}/>
-    </>
-  )
+    <RecipeContext.Provider value={recipeContextValue}>
+      <RecipeList recipes={recipes} />
+    </RecipeContext.Provider>
+  );
 }
 
 const sampleRecipes = [
@@ -16,15 +50,12 @@ const sampleRecipes = [
     name: "Plain Chicken",
     servings: 3,
     cookTime: "1.45",
-    instructions: "1. Put salt on chicken\n2. Put chicken in oven\n3. Eat chicken",
+    instructions:
+      "1. Put salt on chicken\n2. Put chicken in oven\n3. Eat chicken",
     ingredients: [
-      {id: "1",
-      name: "Salt",
-      amount: "1 Tbs"},
-      {id: "2",
-      name: "Chicken",
-      amount: "2 pounds"},
-    ]
+      { id: "1", name: "Salt", amount: "1 Tbs" },
+      { id: "2", name: "Chicken", amount: "2 pounds" },
+    ],
   },
   {
     id: 2,
@@ -33,15 +64,10 @@ const sampleRecipes = [
     cookTime: "0.45",
     instructions: "1. Put pepperica on pork\n2. Put pork in oven\n3. Eat pork",
     ingredients: [
-      {id: "1",
-      name: "Pepperica",
-      amount: "2 Tbs"},
-      {id: "2",
-      name: "Pork",
-      amount: "2 pounds"},
-    ]
+      { id: "1", name: "Pepperica", amount: "2 Tbs" },
+      { id: "2", name: "Pork", amount: "2 pounds" },
+    ],
   },
-
-]
+];
 
 export default App;
--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,36 +1,52 @@
-import React from 'react'
-import IngredientList from './IngredientList'
+import React, { useContext } from "react";
+import IngredientList from "./IngredientList";
 
-export default function Recipe({name, servings, cookTime, instructions, ingredients}) {
+import { RecipeContext } from "./App";
+
+export default function Recipe({
+  id,
+  name,
+  servings,
+  cookTime,
+  instructions,
+  ingredients,
+}) {
+  const { handleRecipeDelete } = useContext(RecipeContext);
+
   return (
     <div className="recipe">
-        <div className="recipe__header">
-            <h3 className="recipe__title">{name}</h3>
-            <div>
-                <button className="btn btn--primary mr-1">Edit</button>
-                <button className="btn btn--danger">Delete</button>
-            </div>
-        </div>
-        <div className="recipe__row">
-            <span className="recipe__label">Cook time:</span>
-            <span className="recipe__value">{cookTime}</span>
-        </div>
-        <div className="recipe__row">
-            <span>Servings:</span>
-            <span>{servings}</span>
-        </div>
-        <div className="recipe__row">
-            <span className="recipe__label">Instructions:</span>
-            <div className="recipe__value recipe__value--instructions recipe__value--indented">
-                {instructions}
-            </div>
-        </div>
+      <div className="recipe__header">
+        <h3 className="recipe__title">{name}</h3>
         <div>
-            <span className="recipe__label">Ingredients:</span>
-            <div className="recipe__value recipe__value--indented">
-            <IngredientList ingredients={ingredients}/>
-            </div>
+          <button className="btn btn--primary mr-1">Edit</button>
+          <button
+            className="btn btn--danger"
+            onClick={() => handleRecipeDelete(id)}
+          >
+            Delete
+          </button>
         </div>
+      </div>
+      <div className="recipe__row">
+        <span className="recipe__label">Cook time:</span>
+        <span className="recipe__value">{cookTime}</span>
+      </div>
+      <div className="recipe__row">
+        <span>Servings:</span>
+        <span>{servings}</span>
+      </div>
+      <div className="recipe__row">
+        <span className="recipe__label">Instructions:</span>
+        <div className="recipe__value recipe__value--instructions recipe__value--indented">
+          {instructions}
+        </div>
+      </div>
+      <div>
+        <span className="recipe__label">Ingredients:</span>
+        <div className="recipe__value recipe__value--indented">
+          <IngredientList ingredients={ingredients} />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
